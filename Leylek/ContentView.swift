6478a7859f681262037e4960befa839abb360608
murathankarasu <murathankarasu@icloud.com>
2025-02-25 19:15:45 +0300

    First Commit

---
+++ Leylek/ContentView.swift
@@ -0,0 +1,13 @@
+//
+//  ContentView.swift
+//  Leylek
+//
+//  Created by Murathan Karasu on 25.02.2025.
+//
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+    }
+}
